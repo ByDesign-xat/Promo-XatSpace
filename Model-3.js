356a3017586e51a19cd862333d1b9af79f2dc5d9
ByDesign-xat <97501590+ByDesign-xat@users.noreply.github.com>
2026-05-20 19:59:16 +0100

    Add files via upload

---
+++ Model-3.js
@@ -0,0 +1,19 @@
+document.addEventListener('keydown', function(e) {
+
+  if (e.key === 'F12') {
+    e.preventDefault();
+  }
+
+  if (e.ctrlKey && e.key.toLowerCase() === 'u') {
+    e.preventDefault();
+  }
+
+  if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === 'i') {
+    e.preventDefault();
+  }
+
+});
+
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+});
